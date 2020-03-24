7d7f04e366ccc730f0081f22d7e0269489b08b37
Sagar Vaddoriya <vaddoriy@sheridancollege.ca>
2020-03-24 19:32:35 -0400

    initial commit

---
+++ A1SagarVaddoriya/A1SagarVaddoriya/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace A1SagarVaddoriya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbBeverage = new System.Windows.Forms.ComboBox();
+            this.cmbAppetizer = new System.Windows.Forms.ComboBox();
+            this.cmbMainCourse = new System.Windows.Forms.ComboBox();
+            this.cmbDessert = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSubTotal = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restaurant Bill Calculator";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(56, 407);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 123);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Info ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name:- SAGAR VADDORIYA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "id:- 991538877";
+            // 
+            // cmbBeverage
+            // 
+            this.cmbBeverage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBeverage.FormattingEnabled = true;
+            this.cmbBeverage.Items.AddRange(new object[] {
+            "Select Your Beverage",
+            "Soda -$1.95",
+            "Tea -$1.50",
+            "Coffe -$1.25",
+            "Mineral Water -$2.95",
+            "Juice -$2.50"});
+            this.cmbBeverage.Location = new System.Drawing.Point(117, 42);
+            this.cmbBeverage.Name = "cmbBeverage";
+            this.cmbBeverage.Size = new System.Drawing.Size(202, 28);
+            this.cmbBeverage.TabIndex = 2;
+            this.cmbBeverage.SelectedIndexChanged += new System.EventHandler(this.cmbBeverage_SelectedIndexChanged);
+            // 
+            // cmbAppetizer
+            // 
+            this.cmbAppetizer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAppetizer.FormattingEnabled = true;
+            this.cmbAppetizer.Items.AddRange(new object[] {
+            "Select Your Appetizer",
+            "Buffalo wings -$5.95",
+            "Buffalo fingers -$6.95",
+            "Nachos -$8.95",
+            "Mashroom caps -$10.95",
+            "Chips and Salsa -$6.95"});
+            this.cmbAppetizer.Location = new System.Drawing.Point(117, 90);
+            this.cmbAppetizer.Name = "cmbAppetizer";
+            this.cmbAppetizer.Size = new System.Drawing.Size(202, 28);
+            this.cmbAppetizer.TabIndex = 3;
+            this.cmbAppetizer.SelectedIndexChanged += new System.EventHandler(this.cmbAppetizer_SelectedIndexChanged);
+            // 
+            // cmbMainCourse
+            // 
+            this.cmbMainCourse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMainCourse.FormattingEnabled = true;
+            this.cmbMainCourse.Items.AddRange(new object[] {
+            "Select your Main Course",
+            "Chicken alfredo -$13.95",
+            "Turkey clud -$11.95",
+            "Prime rib -$20.95",
+            "Turkey dinner -$13.95",
+            "Stuffed chicken -$14.95"});
+            this.cmbMainCourse.Location = new System.Drawing.Point(117, 144);
+            this.cmbMainCourse.Name = "cmbMainCourse";
+            this.cmbMainCourse.Size = new System.Drawing.Size(202, 28);
+            this.cmbMainCourse.TabIndex = 4;
+            this.cmbMainCourse.SelectedIndexChanged += new System.EventHandler(this.cmbMainCourse_SelectedIndexChanged);
+            // 
+            // cmbDessert
+            // 
+            this.cmbDessert.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDessert.FormattingEnabled = true;
+            this.cmbDessert.Items.AddRange(new object[] {
+            "Select Your Dessert",
+            "Apple pie -$5.95",
+            "Sundae -$3.95",
+            "Carrot cake -$5.95",
+            "Apple crisp -$5.95"});
+            this.cmbDessert.Location = new System.Drawing.Point(117, 193);
+            this.cmbDessert.Name = "cmbDessert";
+            this.cmbDessert.Size = new System.Drawing.Size(202, 28);
+            this.cmbDessert.TabIndex = 5;
+            this.cmbDessert.SelectedIndexChanged += new System.EventHandler(this.cmbDessert_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Beverages:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Appetizers:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Main Courses:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Desserts:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cmbDessert);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.cmbMainCourse);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cmbAppetizer);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.cmbBeverage);
+            this.groupBox2.Location = new System.Drawing.Point(56, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(367, 264);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Menu Items";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(493, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 264);
+            this.listBox1.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(510, 395);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "SubTotal:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(510, 433);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Tax@13%:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(510, 471);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Total:";
+            // 
+            // txtSubTotal
+            // 
+            this.txtSubTotal.Enabled = false;
+            this.txtSubTotal.Location = new System.Drawing.Point(636, 395);
+            this.txtSubTotal.Name = "txtSubTotal";
+            this.txtSubTotal.Size = new System.Drawing.Size(100, 26);
+            this.txtSubTotal.TabIndex = 15;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Enabled = false;
+            this.txtTax.Location = new System.Drawing.Point(636, 433);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(100, 26);
+            this.txtTax.TabIndex = 16;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(636, 471);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 26);
+            this.txtTotal.TabIndex = 17;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(636, 517);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 33);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "Clear All";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.txtSubTotal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBeverage;
+        private System.Windows.Forms.ComboBox cmbAppetizer;
+        private System.Windows.Forms.ComboBox cmbMainCourse;
+        private System.Windows.Forms.ComboBox cmbDessert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtSubTotal;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ A1SagarVaddoriya/A1SagarVaddoriya/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A1SagarVaddoriya
+{
+    public partial class Form1 : Form
+    {
+        double subTotal = 0;
+        double tax = 0;
+        double total = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbAppetizer.SelectedIndex = 0;
+            cmbBeverage.SelectedIndex = 0;
+            cmbMainCourse.SelectedIndex = 0;
+            cmbDessert.SelectedIndex = 0;
+        }
+
+        private void cmbBeverage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if(cmbBeverage.SelectedIndex > 0)
+            {
+                listBox1.Items.Add(cmbBeverage.SelectedItem);
+
+                if(cmbBeverage.SelectedIndex == 1)
+                {
+                    subTotal += 1.95;
+
+                }
+                else if (cmbBeverage.SelectedIndex == 2)
+                {
+                    subTotal += 1.50;
+                }
+                else if (cmbBeverage.SelectedIndex == 3)
+                {
+                    subTotal += 1.25;
+                }
+                else if (cmbBeverage.SelectedIndex == 4)
+                {
+                    subTotal += 2.95;
+                }
+                else
+                {
+                    subTotal += 2.50;
+                }
+            }
+            txtSubTotal.Text = subTotal.ToString();
+            tax = subTotal*13/100;
+            txtTax.Text = tax.ToString();
+            total = subTotal + tax;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void cmbAppetizer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbAppetizer.SelectedIndex > 0)
+            {
+                listBox1.Items.Add(cmbAppetizer.SelectedItem);
+                if (cmbAppetizer.SelectedIndex == 1)
+                {
+                    subTotal += 5.95;
+
+                }
+                else if (cmbAppetizer.SelectedIndex == 2)
+                {
+                    subTotal += 6.95;
+                }
+                else if (cmbAppetizer.SelectedIndex == 3)
+                {
+                    subTotal += 8.95;
+                }
+                else if (cmbAppetizer.SelectedIndex == 4)
+                {
+                    subTotal += 10.95;
+                }
+                else
+                {
+                    subTotal += 6.95;
+                }
+            }
+            txtSubTotal.Text = subTotal.ToString();
+            tax = subTotal * 13 / 100;
+            txtTax.Text = tax.ToString();
+            total = subTotal + tax;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void cmbMainCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbMainCourse.SelectedIndex > 0)
+            {
+                listBox1.Items.Add(cmbMainCourse.SelectedItem);
+                if (cmbMainCourse.SelectedIndex == 1)
+                {
+                    subTotal += 13.95;
+
+                }
+                else if (cmbMainCourse.SelectedIndex == 2)
+                {
+                    subTotal += 11.95;
+                }
+                else if (cmbMainCourse.SelectedIndex == 3)
+                {
+                    subTotal += 20.95;
+                }
+                else if (cmbMainCourse.SelectedIndex == 4)
+                {
+                    subTotal += 13.95;
+                }
+                else
+                {
+                    subTotal += 14.95;
+                }
+            }
+            txtSubTotal.Text = subTotal.ToString();
+            tax = subTotal * 13 / 100;
+            txtTax.Text = tax.ToString();
+            total = subTotal + tax;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void cmbDessert_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbDessert.SelectedIndex > 0)
+            {
+                listBox1.Items.Add(cmbDessert.SelectedItem);
+                if (cmbDessert.SelectedIndex == 1)
+                {
+                    subTotal += 5.95;
+
+                }
+                else if (cmbDessert.SelectedIndex == 2)
+                {
+                    subTotal += 3.95;
+                }
+                else if (cmbDessert.SelectedIndex == 3)
+                {
+                    subTotal += 5.95;
+                }
+               
+                else
+                {
+                    subTotal += 5.95;
+                }
+            }
+            txtSubTotal.Text = subTotal.ToString();
+            tax = subTotal * 13 / 100;
+            txtTax.Text = tax.ToString();
+            total = subTotal + tax;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cmbAppetizer.SelectedIndex = 0;
+            cmbBeverage.SelectedIndex = 0;
+            cmbMainCourse.SelectedIndex = 0;
+            cmbDessert.SelectedIndex = 0;
+            listBox1.Items.Clear();
+            subTotal = 0;
+            tax = 0;
+            total = 0;
+            txtSubTotal.Text = "0";
+            txtTax.Text = "0";
+            txtTotal.Text = "0";
+
+
+        }
+    }
+}
